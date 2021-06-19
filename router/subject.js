c30a533fd57bf0fccc7e35fc09add828efa4a7f7
Yasin Arafat <mdyasinarafat3@gmail.com>
2021-06-19 22:33:07 +0200

    get mean grade of tests

--- router/subject.js
+++ router/subject.js
@@ -9,5 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 router.get("/all_subjects", subjectController.all_subject);
 router.post("/singleSubject/:Id", subjectController.single_subject);
+router.get("/userSubjects/:Id", subjectController.user_subjects);
+router.get("/userSubjectsMeanResults/:Id", subjectController.user_subjects_test_mean);
 
 module.exports = router;
