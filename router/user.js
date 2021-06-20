49cb75bed5c50491264f47d38c4f9052886bf03e
Sumon SUtrodhar <24304603+stdr-sumon@users.noreply.github.com>
2021-06-20 03:08:37 +0200

    added 11 page

--- router/user.js
+++ router/user.js
@@ -16,7 +16,7 @@ router.get("/users", userController.all_users);
 router.post("/users", userController.addUser);
 router.get("/login", userController.getLogin);
 router.post("/login", userController.login);
-router.post("/register",userController.addUser);
+router.post("/register", userController.addUser);
 router.get("/users/:Id", userController.edit_user)
 router.put("/users/:Id", userController.update_user)
 router.delete("/users/:Id", userController.delete_user)
@@ -25,5 +25,14 @@ router.get("/users/assignAvailableList/:Id", userController.list_of_assign_avail
 router.post("/users/assignStudent/:Id", userController.assign_student_a_class)
 
 router.get("/api/v1/all_subjects", subjectController.all_subject);
+
+router.get("/users/sub_list/:Id", userController.list_all_subject);
+//router.post("/users/add_subject/:Id", userController.addEditSubject);
+router.post("/users/add_edit_subject/:Id/:operation_type", userController.addEditSubject);
+router.post("/users/archive_subject/:Id/:subject_id", userController.archiveSubject);
+router.post("/users/delete_subject/:Id/:subject_id", userController.deleteSubject);
+router.get("/users/edit_subject/:Id/:subject_id", userController.showSubjectInfo);
+
+
 
 module.exports = router;
